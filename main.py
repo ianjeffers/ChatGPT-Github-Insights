f6d6323c70f9cc2f1f3d21e59994d01eccb37789
ianjeffers <71468572+ianjeffers@users.noreply.github.com>
2023-05-02 15:22:32 -0700

    Initial launch-- limited features. Intelligent context selection, code retrieval, and basic URL authentication.

--- main.py
+++ main.py
@@ -1,33 +1,116 @@
 import json
-
+import os
 import quart
 import quart_cors
 from quart import request
+import httpx
+from quart import session
+from quart_session import Session
+from dotenv import load_dotenv
+import uuid
+
+load_dotenv()
+
+GITHUB_CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.environ.get("GITHUB_CLIENT_SECRET")
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
+# Configure session
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secret_key_for_demo_purposes")
+app.config["SESSION_TYPE"] = "redis"
+Session(app)
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
+async def github_api_request(url, access_token):
+    headers = {
+        "Authorization": f"token {access_token}",
+        "Accept": "application/vnd.github+json",
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+    return response.json()
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
+async def get_repository_info(repo_name, access_token, properties=None):
+    url = f"https://api.github.com/repos/{repo_name}"
+    repo_info = await github_api_request(url, access_token)
+    if properties:
+        repo_info = {k: v for k, v in repo_info.items() if k in properties}
+    return repo_info
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
+async def get_github_user(access_token):
+    url = "https://api.github.com/user"
+    user_info = await github_api_request(url, access_token)
+    return user_info
+
+@app.get("/repository_info/<string:username>/<string:repo_name>")
+async def repository_info(username, repo_name):
+    if "access_token" not in session:
+        return quart.Response(response='User access token not found', status=401)
+
+    access_token = session["access_token"]
+    properties = request.args.get("properties")
+
+    if properties:
+        properties = properties.split(",")
+
+    full_repo_name = f"{username}/{repo_name}"
+    repo_info = await get_repository_info(full_repo_name, access_token, properties)
+    return quart.Response(response=json.dumps(repo_info), status=200)
+
+async def get_repository_file_contents(repo_name, file_path, access_token):
+    url = f"https://api.github.com/repos/{repo_name}/contents/{file_path}"
+    file_info = await github_api_request(url, access_token)
+    return file_info
+
+
+@app.route("/generate_auth_url")
+async def generate_auth_url():
+    state = os.urandom(16).hex()
+    session["state"] = state
+    auth_url = f"https://github.com/login/oauth/authorize?client_id={GITHUB_CLIENT_ID}&scope=repo&redirect_uri=https://{request.headers['Host']}/callback&state={state}"
+    return quart.Response(response=json.dumps({"auth_url": auth_url}), status=200)
+
+
+@app.get("/repository_file_contents/<string:repo_name>/<path:file_path>")
+async def repository_file_contents(repo_name, file_path):
+    if "access_token" not in session:
+        return quart.Response(response='User access token not found', status=401)
+
+    access_token = session["access_token"]
+    file_info = await get_repository_file_contents(repo_name, file_path, access_token)
+    return quart.Response(response=json.dumps(file_info), status=200)
+
+@app.route("/callback")
+async def callback():
+    received_state = request.args.get("state")
+    if "state" not in session or session["state"] != received_state:
+        return quart.Response(response='Invalid state parameter', status=400)
+
+    code = request.args.get("code")
+    token_url = "https://github.com/login/oauth/access_token"
+    data = {
+        "client_id": GITHUB_CLIENT_ID,
+        "client_secret": GITHUB_CLIENT_SECRET,
+        "code": code,
+        "redirect_uri": f"https://{request.headers['Host']}/callback",
+        "state": session["state"],
+    }
+    headers = {"Accept": "application/json"}
+    async with httpx.AsyncClient() as client:
+        response = await client.post(token_url, data=data, headers=headers)
+
+    if response.status_code != 200:
+        return quart.Response(response='Failed to get access token', status=400)
+
+    access_token = response.json()["access_token"]
+
+    # Fetch the user's GitHub profile
+    user_info = await get_github_user(access_token)
+    username = user_info["login"]
+
+    # Store the access token in a secure session
+    session["access_token"] = access_token
+
     return quart.Response(response='OK', status=200)
 
 @app.get("/logo.png")
