53e2d259bf599e973ec3465bb1126ff0d82bcb5e
ianjeffers <71468572+ianjeffers@users.noreply.github.com>
2023-05-03 17:20:08 -0700

    Functional program with Github OAuth, repo file contents, file content!

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import json
 import os
+import uuid
 import quart
+import httpx
+import aioredis
 import quart_cors
 from quart import request
-import httpx
-from quart import session
-from quart_session import Session
 from dotenv import load_dotenv
-import uuid
+from quart_session import Session
 
 load_dotenv()
 
 GITHUB_CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.environ.get("GITHUB_CLIENT_SECRET")
+CALLBACK_PATH = "https://b2d0-2600-1700-7c00-6df0-3921-cadb-59a-c723.ngrok-free.app/callback"
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Configure session
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secret_key_for_demo_purposes")
 app.config["SESSION_TYPE"] = "redis"
+
+redis = aioredis.from_url("redis://localhost", encoding="utf-8", decode_responses=True)
 Session(app)
 
 async def github_api_request(url, access_token):
@@ -33,57 +35,57 @@ async def github_api_request(url, access_token):
 async def get_repository_info(repo_name, access_token, properties=None):
     url = f"https://api.github.com/repos/{repo_name}"
     repo_info = await github_api_request(url, access_token)
-    if properties:
-        repo_info = {k: v for k, v in repo_info.items() if k in properties}
-    return repo_info
-
-async def get_github_user(access_token):
-    url = "https://api.github.com/user"
-    user_info = await github_api_request(url, access_token)
-    return user_info
+    return {k: v for k, v in repo_info.items() if k in properties} if properties else repo_info
 
 @app.get("/repository_info/<string:username>/<string:repo_name>")
 async def repository_info(username, repo_name):
-    if "access_token" not in session:
+    access_token = await redis.get("access_token")
+    if access_token is None:
         return quart.Response(response='User access token not found', status=401)
-
-    access_token = session["access_token"]
-    properties = request.args.get("properties")
-
-    if properties:
-        properties = properties.split(",")
-
-    full_repo_name = f"{username}/{repo_name}"
-    repo_info = await get_repository_info(full_repo_name, access_token, properties)
+    properties = request.args.get("properties", "").split(",")
+    repo_info = await get_repository_info(f"{username}/{repo_name}", access_token, properties)
     return quart.Response(response=json.dumps(repo_info), status=200)
 
-async def get_repository_file_contents(repo_name, file_path, access_token):
-    url = f"https://api.github.com/repos/{repo_name}/contents/{file_path}"
-    file_info = await github_api_request(url, access_token)
-    return file_info
+async def list_repository_directory(username, repo_name, file_path, access_token):
+    url = f"https://api.github.com/repos/{username}/{repo_name}/contents/{file_path}"
+    return await github_api_request(url, access_token)
 
+async def list_repository_directory_endpoint(username, repo_name, file_path=""):
+    access_token = await redis.get("access_token")
+    if access_token is None:
+        return quart.Response(response='User access token not found', status=401)
+    directory_contents = await list_repository_directory(username, repo_name, file_path, access_token)
+    return quart.Response(response=json.dumps(directory_contents), status=200)
+
+@app.get("/list_repository_directory/<string:username>/<string:repo_name>")
+@app.get("/list_repository_directory/<string:username>/<string:repo_name>/<string:file_path>")
+async def list_repository_directory_route(username, repo_name, file_path=""):
+    return await list_repository_directory_endpoint(username, repo_name, file_path)
+
+async def get_repository_file_contents(username, repo_name, file_path, access_token):
+    url = f"https://api.github.com/repos/{username}/{repo_name}/contents/{file_path}"
+    return await github_api_request(url, access_token)
+
+@app.get("/repository_file_contents/<string:username>/<string:repo_name>/<path:file_path>")
+async def repository_file_contents(username, repo_name, file_path):
+    access_token = await redis.get("access_token")
+    if access_token is None:
+        return quart.Response(response='User access token not found', status=401)
+    file_info = await get_repository_file_contents(username, repo_name, file_path, access_token)
+    return quart.Response(response=json.dumps(file_info), status=200)
 
 @app.route("/generate_auth_url")
 async def generate_auth_url():
     state = os.urandom(16).hex()
-    session["state"] = state
-    auth_url = f"https://github.com/login/oauth/authorize?client_id={GITHUB_CLIENT_ID}&scope=repo&redirect_uri=https://{request.headers['Host']}/callback&state={state}"
+    await redis.set("state", state)
+    auth_url = f"https://github.com/login/oauth/authorize?client_id={GITHUB_CLIENT_ID}&scope=repo&redirect_uri={CALLBACK_PATH}&state={state}"
     return quart.Response(response=json.dumps({"auth_url": auth_url}), status=200)
-
-
-@app.get("/repository_file_contents/<string:repo_name>/<path:file_path>")
-async def repository_file_contents(repo_name, file_path):
-    if "access_token" not in session:
-        return quart.Response(response='User access token not found', status=401)
-
-    access_token = session["access_token"]
-    file_info = await get_repository_file_contents(repo_name, file_path, access_token)
-    return quart.Response(response=json.dumps(file_info), status=200)
 
 @app.route("/callback")
 async def callback():
     received_state = request.args.get("state")
-    if "state" not in session or session["state"] != received_state:
+    session_state = await redis.get("state")
+    if session_state != received_state:
         return quart.Response(response='Invalid state parameter', status=400)
 
     code = request.args.get("code")
@@ -92,8 +94,8 @@ async def callback():
         "client_id": GITHUB_CLIENT_ID,
         "client_secret": GITHUB_CLIENT_SECRET,
         "code": code,
-        "redirect_uri": f"https://{request.headers['Host']}/callback",
-        "state": session["state"],
+        "redirect_uri": CALLBACK_PATH,
+        "state": session_state,
     }
     headers = {"Accept": "application/json"}
     async with httpx.AsyncClient() as client:
@@ -103,15 +105,12 @@ async def callback():
         return quart.Response(response='Failed to get access token', status=400)
 
     access_token = response.json()["access_token"]
-
-    # Fetch the user's GitHub profile
-    user_info = await get_github_user(access_token)
-    username = user_info["login"]
-
-    # Store the access token in a secure session
-    session["access_token"] = access_token
-
+    await redis.set("access_token", access_token)
     return quart.Response(response='OK', status=200)
+
+async def get_github_user(access_token):
+    url = "https://api.github.com/user"
+    return await github_api_request(url, access_token)
 
 @app.get("/logo.png")
 async def plugin_logo():
@@ -120,17 +119,13 @@ async def plugin_logo():
 
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
-    host = request.headers['Host']
     with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/json")
+        return quart.Response(f.read(), mimetype="text/json")
 
 @app.get("/openapi.yaml")
 async def openapi_spec():
-    host = request.headers['Host']
     with open("openapi.yaml") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/yaml")
+        return quart.Response(f.read(), mimetype="text/yaml")
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
